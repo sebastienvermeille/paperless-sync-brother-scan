2c8b596ac25409aca7abefa376aec3ee4db8be58
Sebastien Vermeille <sebastien.vermeille@geo-satis.com>
2021-06-29 16:02:30 +0200

    Use correct prefix for docker images

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push svermeille/paperlesssyncbrotherscan
+docker push sbeex/paperlesssyncbrotherscan
